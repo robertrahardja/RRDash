195d05df943b582874cf85a2cf153a46b1ee3b0c
Robert Rahardja <robertrahardja@fb.com>
2020-05-07 16:49:42 +0800

    Added Sign in functionality

--- RRDash/RRDash/urls.py
+++ RRDash/RRDash/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rrdashapp import views
+# from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    # path('restaurant/sign-in', auth_views.login, {'template_name': 'restaurant/sign_in.html'}, name='restaurant-sign-in'),
+    path('restaurant/sign-in/', LoginView.as_view(template_name='restaurant/sign_in.html'), name="restaurant-sign-in"),
+    # path('restaurant/sign-out', auth_views.logout, {'next_page': '/'}, name='restaurant-sign-out'),
+    path('restaurant/sign-out/', LogoutView.as_view(next_page='/'), name="restaurant-sign-out"),
+    path('restaurant/', views.restaurant_home, name='restaurant-home'),
+
 ]
