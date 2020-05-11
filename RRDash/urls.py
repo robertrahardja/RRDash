021b047aa7b117caca0253c3bf8904d7a74a02ce
Robert Rahardja <robertrahardja@fb.com>
2020-05-11 12:40:29 +0800

    Added base, account , meal, order and report pages

--- RRDash/urls.py
+++ RRDash/urls.py
@@ -40,5 +40,6 @@ urlpatterns = [
     path('restaurant/account', views.restaurant_account, name='restaurant-account'),
     path('restaurant/meal', views.restaurant_meal, name='restaurant-meal'),
     path('restaurant/order', views.restaurant_order, name='restaurant-order'),
+    path('restaurant/report', views.restaurant_report, name='restaurant-report'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
