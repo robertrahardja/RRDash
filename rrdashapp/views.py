32618345478bc613efaf6641be49be5d6526b1da
Robert Rahardja <robertrahardja@fb.com>
2020-05-12 16:31:14 +0800

    added accounts edit

--- rrdashapp/views.py
+++ rrdashapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from rrdashapp.forms import UserForm, RestaurantForm
+from rrdashapp.forms import UserForm, RestaurantForm, UserFormForEdit
 
 
 # Create your views here.
@@ -14,12 +14,26 @@ def home(request):
 
 @login_required(login_url='sign-in/')
 def restaurant_home(request):
-    return render(request, 'restaurant/home.html', {})
+    return redirect(restaurant_order)
 
 
 @login_required(login_url='sign-in/')
 def restaurant_account(request):
-    return render(request, 'restaurant/account.html', {})
+    user_form = UserFormForEdit(instance=request.user)
+    restaurant_form = RestaurantForm(instance=request.user.restaurant)
+
+    if request.method == "POST":
+        user_form = UserFormForEdit(request.POST, instance=request.user)
+        restaurant_form = RestaurantForm(request.POST, request.FILES, instance=request.user.restaurant)
+
+    if user_form.is_valid() and restaurant_form.is_valid():
+        user_form.save()
+        restaurant_form.save()
+
+    return render(request, 'restaurant/account.html', {
+        "user_form": user_form,
+        "restaurant_form": restaurant_form
+    })
 
 
 @login_required(login_url='sign-in/')
